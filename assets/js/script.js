cb670923d0ef87a8f1ab1f2da25b31398821527a
Lucia Ferencik <lferencikova@gmail.com>
2023-01-02 19:24:21 +0000

    feat: When a color in Options section is clicked, it is added to the first empty square in the row0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// define the available colors in the Options section
 const colorPicker = {
     blue: '#3a2fff',
     pink: '#ff2fa8',
@@ -9,13 +10,32 @@ const colorPicker = {
     orange: '#ffce2f',
 }
 
+// when the dom content is loaded, fill in the predefined colors in the Options section
+document.addEventListener("DOMContentLoaded", function () {
+    let colorChoices = document.getElementsByClassName('color-choice');
 
-document.addEventListener("DOMContentLoaded", function (I) {
-    let colorChoice = document.getElementsByClassName('color-choice');
-
-    for (let i = 0; i < colorChoice.length; i++) {
-        colorChoice[i].style.backgroundColor = Object.values(colorPicker)[i];
+    for (let i = 0; i < colorChoices.length; i++) {
+        colorChoices[i].style.backgroundColor = Object.values(colorPicker)[i];
     }
 
-    // console.log(document.getElementsByClassName('color-choice'));
+    addColorClickedHandlers();
 })
+
+// add onclick event listeners to the squares in the Options section 
+function addColorClickedHandlers() {
+    let colorChoices = document.getElementsByClassName('color-choice');
+    let currentSquares = document.getElementsByClassName('square-row0');
+
+    for (let i = 0; i < colorChoices.length; i++) {
+        colorChoices[i].addEventListener("click", function () {
+            // identify which square in Options section was clicked and assign its color to the first square in row0 which is not yet taken
+            for (let square of currentSquares) {
+                if (!square.classList.contains('is-taken')) {
+                    square.style.backgroundColor = Object.values(colorPicker)[i];
+                    square.classList.add('is-taken');
+                    break;
+                }
+            }
+        })
+    }
+}
