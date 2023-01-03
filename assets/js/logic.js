fd106487cfc74f3dddd091b04c5bc86234eb6a37
Lucia Ferencik <lferencikova@gmail.com>
2023-01-03 23:29:42 +0000

    Add countWhites method and test it

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,13 +3,38 @@ function countBlacks(secret, guess) {
 
     for (let i = 0; i < secret.length; i++)
         if (secret[i] === guess[i]) {
-            blackCounter += 1;
+            blackCounter++;
         }
 
     return blackCounter;
 }
 
+// my husband helped me with this function
+function countWhites(secret, guess) {
+    let whiteCounter = 0;
+    let secretWithoutBlacks = [];
+    let guessWithoutBlacks = [];
+
+    for (let i = 0; i < secret.length; i++) {
+        if (secret[i] !== guess[i]) {
+            secretWithoutBlacks.push(secret[i]);
+            guessWithoutBlacks.push(guess[i]);
+        }
+    }
+
+    while (guessWithoutBlacks.length > 0) {
+        if (secretWithoutBlacks.includes(guessWithoutBlacks[0])) {
+            whiteCounter++;
+            let x = secretWithoutBlacks.indexOf(guessWithoutBlacks[0])
+            secretWithoutBlacks.splice(x, 1);
+        }
+        guessWithoutBlacks.shift();
+    }
+    return whiteCounter;
+}
+
 // https://www.freecodecamp.org/news/how-to-start-unit-testing-javascript/
 module.exports = {
     countBlacks,
+    countWhites
 }
--- assets/js/logic.test.js
+++ assets/js/logic.test.js
@@ -1,6 +1,6 @@
 var logic = require('./logic')
 
-test("Returns all wrong", () => {
+test("Returns no blacks", () => {
     expect(logic.countBlacks(['a', 'b', 'a', 'c', 'd'], ['e', 'e', 'e', 'e', 'e'])).toBe(0);
 });
 
@@ -20,6 +20,30 @@ test("Returns only one black despite repetion in the guess and the secret", () =
     expect(logic.countBlacks(['a', 'b', 'a', 'c', 'd'], ['a', 'a', 'a', 'a', 'a'])).toBe(2);
 });
 
-test("Returs all correct (5 blacks), code guessed", () => {
+test("Returns all blacks (5), code guessed", () => {
     expect(logic.countBlacks(['a', 'b', 'a', 'c', 'd'], ['a', 'b', 'a', 'c', 'd'])).toBe(5);
+});
+
+test("Returns no whites", () => {
+    expect(logic.countWhites(['a', 'b', 'a', 'c', 'd'], ['e', 'e', 'e', 'e', 'e'])).toBe(0);
+});
+
+test("More duplicates in guess than in Secret", () => {
+    expect(logic.countWhites(['a', 'b', 'a', 'c', 'd'], ['e', 'a', 'a', 'a', 'e'])).toBe(1);
+});
+
+test("Same number of duplicates but not at the same position", () => {
+    expect(logic.countWhites(['a', 'b', 'a', 'c', 'd'], ['e', 'a', 'e', 'a', 'e'])).toBe(2);
+});
+
+test("All guesses correct but none in the right position", () => {
+    expect(logic.countWhites(['a', 'b', 'a', 'c', 'd'], ['b', 'a', 'a', 'd', 'c'])).toBe(4);
+});
+
+test("Returns 5 whites", () => {
+    expect(logic.countWhites(['a', 'b', 'a', 'c', 'd'], ['d', 'a', 'b', 'a', 'c'])).toBe(5);
+});
+
+test("Returns 0 whites, 5 blacks", () => {
+    expect(logic.countWhites(['a', 'b', 'a', 'c', 'd'], ['a', 'b', 'a', 'c', 'd'])).toBe(0);
 });
