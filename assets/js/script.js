a8e0d309f1fbee5c1eca094e95e3469b29b9a46b
Lucia Ferencik <lferencikova@gmail.com>
2023-01-04 15:22:40 +0000

    bug: Delete duplicate call of the addButtonClickedHandlers() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     addColorClickedHandlers();
     addButtonClickedHandlers();
-    addButtonClickedHandlers();
     generateNewSecretCode();
 })
 
